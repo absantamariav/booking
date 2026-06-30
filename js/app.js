1940abbeda0ef15a48b4a90c6bc1c1f293ac9ae4
Anderson Santamaria <andersonbsv@gmail.com>
2026-06-30 13:05:52 -0500

    funcion mostrar y ocultar carrito

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const btnFlotante = document.querySelector('.btn-flotante');
 const footer = document.querySelector('.footer');
+const canasta = document.querySelector('.canasta');
+const carrito = document.querySelector('.carrito');
 
-btnFlotante.addEventListener('click', mostrarOcultarFooter);
+cargarEventListeners();
+function cargarEventListeners() {
+    btnFlotante.addEventListener('click', mostrarOcultarFooter);
+    canasta.addEventListener('click', mostrarCarrito);
+}
 
 function mostrarOcultarFooter() {
     if (footer.classList.contains('activo')) {
@@ -14,3 +20,37 @@ function mostrarOcultarFooter() {
         this.textContent = 'X Cerrar';
     }
 }
+
+function mostrarCarrito(e) {
+    e.preventDefault();
+    const carritoActivo = carrito.classList.toggle('activo');
+    if (carritoActivo) {
+        document.addEventListener('click', ocultarCarritoDesdeAfuera);
+        document.addEventListener('keydown', ocultarCarritoConEscape);
+    } else {
+        limpiarEventListeners();
+    }
+}
+
+function ocultarCarritoDesdeAfuera(e) {
+    if (!carrito.contains(e.target) && !canasta.contains(e.target)) {
+        carrito.classList.remove('activo');
+        limpiarEventListeners();
+    }
+}
+
+function ocultarCarritoConEscape(e) {
+    if (e.key === 'Escape') {
+        carrito.classList.remove('activo');
+        canasta.blur();
+        limpiarEventListeners();
+    }
+}
+
+function limpiarEventListeners() {
+    document.removeEventListener('click', ocultarCarritoDesdeAfuera);
+    document.removeEventListener('keydown', ocultarCarritoConEscape);
+}
+
+const listaServicios = document.querySelector('.card');
+let itemsCarrito = [];
